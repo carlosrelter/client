f38ac44029b47a5f6b222450e0a10011d5a3ad36
carlosrelter <reltercontato@gmail.com>
2025-05-12 17:50:34 -0300

    creating side nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,44 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule,
+    MatListModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'client';
+
+  public isSmallScreen =false;
+
+  constructor(private breakpointObserver: BreakpointObserver){}
+
+  ngAfterContentInit():void {
+    this.breakpointObserver.observe(['(max-width: 800px)']).subscribe(
+      (res) => this.isSmallScreen =res.matches
+      // {
+      // next:(res) =>{
+      //   if(res.matches){
+      //     this.isSmallScreen = true;
+      //   }else{
+      //     this.isSmallScreen = false;
+      //   }
+      // }
+    // }
+    );
+  }
+
+  get sidenavMode(){
+    return this.isSmallScreen ? 'over':'side';
+  }
+
 }
